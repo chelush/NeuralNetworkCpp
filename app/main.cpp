88ace9cd50f26ac9b3b2bce802b0dd70b97d17df
Maxim Kirpichev <mkirpichev0316@gmail.com>
2026-02-14 18:05:08 +0300

    Add Eigen 3.4 via FetchContent, demo uses MatrixXf/VectorXf

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,12 @@
+#include <Eigen/Dense>
 #include <iostream>
 
 int main() {
-  std::cout << "NeuralNetwork\n";
+  Eigen::MatrixXf A(2, 2);
+  A << 1, 2, 3, 4;
+  Eigen::VectorXf v(2);
+  v << 1, 0;
+  Eigen::VectorXf u = A * v;
+  std::cout << "NeuralNetwork (Eigen OK): A*v = [" << u(0) << ", " << u(1) << "]\n";
   return 0;
 }
